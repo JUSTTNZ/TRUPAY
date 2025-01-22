b38e4f2e7ab6c5ac56ee1d5801b63fbfdf155faa
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-22 22:56:45 +0100

    updated confidence branch with main branch

---
+++ Backend/models/users.models.js
@@ -0,0 +1,67 @@
+
+import mongoose, {Schema} from "mongoose";
+
+
+const UserSchema = new Schema (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowerCase: true,
+            trim: true
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        registrationNumber: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"]
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'user'],
+            default: 'user'
+        },
+        phoneNumber: {
+            type: Number,
+            required: true,
+            trim: true
+        },
+        school: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        department: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        level: {
+            type: String,
+            required: true,
+            upperCase: true,
+            enum: ['ND1', 'ND2', 'HND1', 'HND2']
+        },
+    },
+    {timestamps: true}
+)
+
+export const user = mongoose.model("User", UserSchema)
