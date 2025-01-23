cf4e8f784ca63b81428d94d54d4ebdb94f49803a
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-23 07:09:36 +0100

    Implement user registration route with validation and error handling

--- Backend/app.js
+++ Backend/app.js
@@ -4,4 +4,12 @@ const app = express();
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
+// routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+app.use("/api/v1/users", userRouter)
+app.use(errorHandler)
+
 export { app }
