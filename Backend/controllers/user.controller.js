2f8491d7064226d743a4d0098cc9a442b559cdaf
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-24 00:04:02 +0100

    Add login functionality to user controller with validation and token generation

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -66,6 +66,58 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 })
 
+const loginUser = asyncHandler(async(req, res) => {
+    const {email, username, registrationNumber, password} = req.body
+
+    if(
+        [email, username, registrationNumber, password].some((field) => field.trim() === "")
+    ) {
+        throw new ApiError(400, "Please provide required fields")
+    }
+
+    let user;
+
+    if(email) {
+        user = await User.findOne({email})
+    } 
+    else if(username) {
+        user = await User.findOne({username})
+    }
+    else if(registrationNumber) {
+        user = await User.findOne({registrationNumber})
+    }
+
+    if(!user) {
+        throw new ApiError(401, "Invalid credentials")
+    }
+
+    try {
+        const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+        const loggedInUser =  await User.findById(user._id).select("-password -refreshToken")
+
+        if(!loggedInUser) {
+            throw new ApiError(401, "Something went wrong while logging in")
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+        }
+
+        return res
+        .status(200)
+        .cookie("refreshToken", refreshToken, options)
+        .cookie("accessToken", accessToken, options)
+        .json(new ApiResponse(200, loggedInUser, "User logged in successfully", {accessToken, refreshToken}
+        ))
+    }    
+     catch (error) {
+        console.log("User login failed", error);
+    }
+})
+
 export {
-    registerUser
+    registerUser,
+    loginUser,
 }
