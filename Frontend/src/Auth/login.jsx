8f6dbdbe3e0e8f36aecccf908f050daefe4ab21d
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-01-23 15:14:37 +0100

    done with login

--- Frontend/src/Auth/login.jsx
+++ Frontend/src/Auth/login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
 
   
     return (
-    <div className="font-[sans-serif]  flex items-center justify-center  p-4 pt-8  h-auto"   style={{
+    <div className="font-[sans-serif]  flex items-center justify-center  p-4 pt-7  h-auto"   style={{
         background: "linear-gradient(239.55deg, #F3C623 -2.42%, #FDC752 27.13%, #10375C 68.51%)",
         
       }}>
