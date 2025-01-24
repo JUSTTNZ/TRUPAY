f3c9bd874fa24da67a4fefb6fd52149f2ae7b4d8
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-24 01:35:56 +0100

    Fix error message in user registration to improve clarity

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -54,7 +54,7 @@ const registerUser = asyncHandler(async (req, res) => {
         const createdUser = await User.findById(user._id).select("-password -refreshToken")
 
         if(!createdUser) {
-            throw new ApiError(401, "Something went wrong while registering user")
+            throw new ApiError(401, "Something went wrong while registering user sorry")
         }
 
         return res
