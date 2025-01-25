c39bd4a1a564781f6466382fddfb97a7bd69f4b3
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-25 12:35:21 +0100

    add logout functionality for users

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express"
+import { verifyJWT}
 import {
     registerUser,
-    loginUser
+    loginUser,
+    logOutUser
 } from "../controllers/user.controller.js"
 
 const router = Router()
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+router.route("/logout").post(logOutUser)
 
 export default router
