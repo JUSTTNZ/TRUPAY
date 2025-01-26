84d5b989088d09df6f786a438e4b8faf2ffa3358
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-26 06:02:20 +0100

    add cookie-parser middleware and update JWT token retrieval logic

--- Backend/middlewares/auth.middlewares.js
+++ Backend/middlewares/auth.middlewares.js
@@ -4,7 +4,7 @@ import { ApiError }  from '../utils/ApiError.js'
 import jwt from "jsonwebtoken"
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
-    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
     console.log(token);
 
     if(!token) {
