9de1073a2e316dac3582230acf39a6d69cedcb0b
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-01-28 13:40:09 +0100

    feat: add mock data for schools, departments, and levels; update populate script

--- Backend/populate.js
+++ Backend/populate.js
@@ -21,3 +21,57 @@ const start = async () => {
 }
 
 start();
+
+
+// require('dotenv').config();
+
+// const mockSchools = require('./MOCK_SCHOOLS.json');
+// const mockDepartments = require('./MOCK_DEPARTMENTS.json');
+// const mockLevels = require('./MOCK_LEVELS.json');
+// const School = require('./models/School');
+// const Department = require('./models/Department');
+// const Level = require('./models/Level');
+// const connectDB = require('./db/connect');
+
+// const start = async () => {
+//     try {
+//         await connectDB(process.env.MONGODB_URI);
+
+//         // Insert mock data for schools
+//         const schools = await School.create(mockSchools);
+//         console.log('Schools data import success');
+
+//         // Insert mock data for departments
+//         const departments = await Department.create(mockDepartments);
+//         console.log('Departments data import success');
+
+//         // Insert mock data for levels
+//         const levels = await Level.create(mockLevels);
+//         console.log('Levels data import success');
+
+//         // Optionally link departments and levels to schools and save again
+//         for (let department of departments) {
+//             const school = schools.find(s => s.name === department.school);
+//             if (school) {
+//                 school.departments.push(department._id);
+//                 await school.save();
+//             }
+//         }
+
+//         for (let level of levels) {
+//             const department = departments.find(d => d.name === level.department);
+//             if (department) {
+//                 department.levels.push(level._id);
+//                 await department.save();
+//             }
+//         }
+
+//         console.log('Data import success');
+//         process.exit(0);
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1);
+//     }
+// }
+
+// start();
