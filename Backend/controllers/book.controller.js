b069c0159fe90bc98f4c037cbc1b17c72afad209
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-02-03 06:03:54 +0100

    Add book management functions for adding and removing books

---
+++ Backend/controllers/book.controller.js
@@ -0,0 +1,46 @@
+import { Book } from '../models/book.model.js'
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+
+const addBooks = asyncHandler(async(req, res) => {
+
+    const { title, author, description, price, stock_quantity, school, department, level} = req.body;
+
+    if(
+        [title, author, description, price, stock_quantity, school, department, level].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(401, "All fields are required")
+    }
+
+    const newBook = await Book.create({
+        title, 
+        author, 
+        description, 
+        price, 
+        stock_quantity, 
+        school, 
+        department, 
+        level
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(201, newBook, "Book added successfully"))
+})
+
+const removeBooks = asyncHandler(async(req, res) => {
+    const {bookId} = req.params
+
+    if(!bookId) {
+        throw new ApiError(401, "BookId is required")
+    }
+
+    const book = await Book.findById(bookId).populate('title level')
+
+})
+
+
+export {
+    addBooks
+}
