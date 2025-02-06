25ec3bd6d9d440fab2ed2629f7fe713ece008da7
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-02-06 07:29:04 +0100

    Update department retrieval to exclude books from the response

--- Backend/controllers/department.controller.js
+++ Backend/controllers/department.controller.js
@@ -51,7 +51,7 @@ const getAllSchoolDepartments = asyncHandler(async(req, res) => {
         }
 
         const schoolId = user.school._id
-        const departments = await Department.find({school: schoolId}).select('-users')
+        const departments = await Department.find({school: schoolId}).select('-users -books')
 
         return res 
         .status(200)
