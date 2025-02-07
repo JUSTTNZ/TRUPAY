0ba6a4cb8b1499061e8bbdcce2446e198b772a54
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-02-07 09:53:02 +0100

    Refactor comment handling: update comment model, enhance book schema to include comments, and modify routes and service for improved functionality

--- Backend/routes/comment.routes.js
+++ Backend/routes/comment.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { verifyJWT } from "jsonwebtoken";
-import { addComments } from "../controllers/comments.controller";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { addComments } from "../controllers/comments.controller.js";
 
 const router = Router()
 
-router.route("/addComment").post(verifyJWT, addComments)
+router.route("/addComment/:bookId").post(verifyJWT, addComments)
 
 export default router
