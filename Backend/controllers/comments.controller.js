3dacbdfafe88516f4790e10caee23ad606c09445
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-02-08 09:16:36 +0100

    Refactor addComments function: remove commented-out console log

--- Backend/controllers/comments.controller.js
+++ Backend/controllers/comments.controller.js
@@ -5,22 +5,21 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
  const addComments = asyncHandler(async (req, res, next) => {
     try {
-        const { bookId } = req.params; // Get book ID from URL params
+        const { bookId } = req.params; 
         //console.log("User from JWT:", req.user); 
-        const userId = req.user._id; // Get user ID from logged-in user
-        const { comment, rating } = req.body; // Get comment & rating from request body
+        const userId = req.user._id;
+        const { comment, rating } = req.body;
 
-        // Ensure both comment and rating are provided
+        
         if (!comment || !rating) {
             throw new ApiError(400, "Comment and rating are required");
         }
 
-        // Call service to create the comment
         const newComment = await CommentService.createComment({ comment, rating }, bookId, userId);
 
         return res.status(201).json(new ApiResponse(201, newComment, "Comment added successfully"));
     } catch (error) {
-        next(error); // Pass error to the error-handling middleware
+        next(error); 
     }
 });
 
