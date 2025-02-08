b5745ce957d334cad86382ac13693224028d67af
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-02-08 10:06:24 +0100

    Refactor comment deletion logic: improve error handling and authorization checks

--- Backend/controllers/comments.controller.js
+++ Backend/controllers/comments.controller.js
@@ -3,7 +3,8 @@ import { Comment } from '../models/comment.model.js';
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
- const addComments = asyncHandler(async (req, res, next) => {
+
+const addComments = asyncHandler(async (req, res, next) => {
     try {
         const { bookId } = req.params; 
         //console.log("User from JWT:", req.user); 
@@ -28,27 +29,29 @@ const deleteComments = asyncHandler(async(req, res) => {
     try {
         const { commentId } = req.params
 
-        if(commentId) {
+        if(!commentId) {
             throw new ApiError(400, "commentId required")
         }
 
-        const comment = await Comment.findByIdAndDelete(commentId).select('book')
-
-        const commentOwner = comment.user
-
-        if(commentOwner._id !== req.user._id) {
-            throw new ApiError(401, "Unauthorized, cant delete comment")
-        } 
+        const comment = await Comment.findByIdAndDelete(commentId).populate('user').select('book user')
 
         if(!comment) {
             throw new ApiError(404, "Comment not found")
         }
 
+        const commentOwnerId = comment.user?._id
+        console.log(req.user._id,);
+        console.log(comment.user?._id)
+        if(!commentOwnerId.equals(req.user._id)) {
+            throw new ApiError(401, "Unauthorized, cant delete comment")
+        } 
+        
         return res
             .status(200)
             .json(new ApiResponse(201, comment, "Comment successfully deleted"))
-    } catch (error) {
-        
+    }catch (error) {
+        console.log(error);
+        throw new ApiError(400, "An error occurred")
     }
 })
 
