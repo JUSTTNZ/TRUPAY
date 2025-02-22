c5ffd23267c63b0c851f79fe663ced9df76cef6b
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-02-22 15:55:48 +0100

    started the payment page

---
+++ Frontend/src/component/Payment/cart.jsx
@@ -0,0 +1,14 @@
+export const Cart= () => {
+    return (
+        <>
+           <div className="bg-[#10375C] text-[#F4F6FF] min-h-[calc(100vh-4rem)] mt-16">
+              <div className="container mx-auto pt-20">
+                <div className="">
+                    <img src={} alt="" />
+                    <p className="">Continue Shopping</p>
+                </div>
+              </div>
+           </div> 
+        </>
+    )
+}
